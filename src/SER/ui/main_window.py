3b957574d09b53af7439b0bc3e03fa06ac00e31e
Francisco Xavier Gauna <fgauna@fi.uba.ar>
2023-09-18 17:28:09 -0300

    tabla

--- src/SER/ui/main_window.py
+++ src/SER/ui/main_window.py
@@ -4,10 +4,11 @@ from typing import Collection
 from logging import getLogger as get_logger
 
 from PyQt5 import uic
-from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtWidgets import QGroupBox, QGridLayout, QPushButton, QProgressBar, QLabel, QStackedWidget
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+from PyQt5.QtWidgets import QGroupBox, QGridLayout, QPushButton, QProgressBar, QLabel, QStackedWidget, QTableView
 from pimpmyclass.mixins import LogMixin
 
+from .data_table import TableModel
 from .progress_tracker import ProgressTracker
 from ..interfaces import ComponentInitialization
 from ..model.runner import ExperimentRunner
@@ -15,6 +16,8 @@ from ..model.runner import ExperimentRunner
 
 class MainWidget(QStackedWidget, LogMixin):
     progress_changed = pyqtSignal()
+    progress_ended = pyqtSignal()
+
     conf_layout: QGridLayout
     run_layout: QGridLayout
     data_layout: QGridLayout
@@ -24,6 +27,8 @@ class MainWidget(QStackedWidget, LogMixin):
     progress_tracker: ProgressTracker
     progress_bar: QProgressBar
     progress_label: QLabel
+
+    data_table: QTableView
 
     conf_page: QStackedWidget
     run_page: QStackedWidget
@@ -36,21 +41,33 @@ class MainWidget(QStackedWidget, LogMixin):
 
     def __init__(self, components: Collection[ComponentInitialization], runner: ExperimentRunner):
         super().__init__()
+
+        # This loads the file and loads up each object as part of this class
+        # When using this method it's important to not overlap names with the widget
+        # as the class will put the widgets as direct attributes of MainWidget
         ui_file_path = path.join(path.dirname(path.realpath(__file__)), "main_widget.ui")
         uic.loadUi(ui_file_path, self)
 
-        self.conf_layout = self.conf_box.layout()
-        self.run_layout = self.run_box.layout()
-        self.data_layout = self.data_box.layout()
-
+        # Creating Runner
         self.components = components
         self.runner = runner
         self.run_thread = Thread(target=self.run_experiment)
         self.started = False
         self.logger = get_logger("SER.Core.MainWindow")
+
+        # Loading GUI
+        self.conf_layout = self.conf_box.layout()
+        self.run_layout = self.run_box.layout()
+        self.data_layout = self.data_box.layout()
+
         self.load_config_gui()
         self.load_run_gui()
         self.load_data_gui()
+
+        # Connecting Slots
+        self.progress_changed.connect(self.progress_change)
+        self.progress_ended.connect(self.progress_end)
+
         self.show()
 
     def load_config_gui(self):
@@ -67,7 +84,6 @@ class MainWidget(QStackedWidget, LogMixin):
     def load_run_gui(self):
         self.log_debug(msg="Started loading run interface")
         self.progress_tracker = ProgressTracker(self.progress_bar, self.progress_label)
-        self.progress_changed.connect(self.progress_tracker.advance)
 
     def load_data_gui(self):
         self.log_debug(msg="Started loading data interface")
@@ -81,10 +97,24 @@ class MainWidget(QStackedWidget, LogMixin):
             self.started = True
             self.log_debug(msg="Changing interface to the experiment interface")
             self.setCurrentWidget(self.run_page)
-            self.progress_tracker.start(self.runner.arg_tracker.points_amount())
+            self.progress_start()
             self.run_thread.start()
 
     def run_experiment(self):
         self.log_debug(msg="Changing interface to the data interface")
         self.runner.run_experiment(self.progress_changed.emit)
+        self.progress_ended.emit()
+
+    def progress_start(self):
+        self.progress_tracker.start(self.runner.arg_tracker.points_amount())
+
+    @pyqtSlot()
+    def progress_change(self):
+        self.progress_tracker.advance()
+        # We also execute here the advancements of the data class
+
+    @pyqtSlot()
+    def progress_end(self):
+        self.data_model = TableModel(self.runner.data.to_dataframe())
+        self.data_table.setModel(self.data_model)
         self.setCurrentWidget(self.data_page)
