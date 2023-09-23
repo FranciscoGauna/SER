42faba104c26439bec41327adfc4f69335900ede
Francisco Xavier Gauna <fgauna@fi.uba.ar>
2023-09-22 21:46:21 -0300

    mas cambios

--- preprod/pixmap_test.py
+++ preprod/pixmap_test.py
@@ -12,6 +12,7 @@ from PyQt5.QtWidgets import QGroupBox, QApplication, QDoubleSpinBox, QPushButton
 thread_device = QThread()
 thread_device.start()
 
+
 class PixmapText(QGroupBox):
     add_value_button: QPushButton
     value_spin_box: QDoubleSpinBox
@@ -20,7 +21,7 @@ class PixmapText(QGroupBox):
     points: List[float]
     min_val: float
     max_val: float
-    hsv_min = 66, 0.18, 1
+    hsv_min = 0.1833, 0.18, 1
     hsv_max = 0, 0.85, 1
     width = 10
     height = 10
@@ -34,7 +35,11 @@ class PixmapText(QGroupBox):
         self.max_val = -inf
 
         self.add_value_button.pressed.connect(self.add_value)
+        self.zoom_in_button.pressed.connect(self.view_zoom_in)
+        self.zoom_out_button.pressed.connect(self.view_zoom_out)
+        self.zoom_reset_button.pressed.connect(self.view_zoom_reset)
 
+        self.current_zoom = 1
         self.q_image = QImage(self.width, self.height, QImage.Format_RGB32)
         self.q_image.fill(QColor.fromHsvF(0, 1, 0))
         self.q_pixmap = QPixmap()
@@ -51,7 +56,10 @@ class PixmapText(QGroupBox):
             self.min_val = value
         if self.max_val < value:
             self.max_val = value
-        print(self.points)
+
+        if self.min_val == self.max_val:
+            for i in range(len(self.points)):
+                self.set_color(*self.hsv_min, i)
 
     def calculate_color(self, value) -> Tuple[float, float, float]:
         return 0, 0, 1
@@ -62,7 +70,6 @@ class PixmapText(QGroupBox):
         x = index % self.width
         y = int((index - x) / self.width)
 
-        print(x, y)
         self.q_image.setPixelColor(x, y, QColor.fromHsvF(h, s, v))
 
     def add_value(self):
@@ -75,10 +82,23 @@ class PixmapText(QGroupBox):
         self.reload_image()
 
     def reload_image(self):
-        self.scene.removeItem(self.g_pixmap)
         self.q_pixmap.convertFromImage(self.q_image)
-        self.g_pixmap = QGraphicsPixmapItem(self.q_pixmap)
-        self.scene.addItem(self.g_pixmap)
+        self.g_pixmap.setPixmap(self.q_pixmap)
+        self.g_pixmap.update()
+
+    def view_zoom_in(self):
+        scale = 2
+        self.current_zoom *= scale
+        self.map_view.scale(scale, scale)
+
+    def view_zoom_out(self):
+        scale = 0.5
+        self.current_zoom *= scale
+        self.map_view.scale(scale, scale)
+
+    def view_zoom_reset(self):
+        self.map_view.scale(1 / self.current_zoom, 1 / self.current_zoom)
+        self.current_zoom = 1
 
 
 if __name__ == '__main__':
